2bd7f3653defb3ef29245b7b344deccc69033189
Mengxiong <mliu60@illinois.edu>
2018-11-15 23:21:39 -0500

    fix lambda identity

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -21,7 +21,7 @@ class CycleGANModel(BaseModel):
             parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
             parser.add_argument('--lambda_B', type=float, default=10.0,
                                 help='weight for cycle loss (B -> A -> B)')
-            parser.add_argument('--lambda_identities', type=dict, default={'facades': 0.5, 'mini': 0.0},
+            parser.add_argument('--lambda_identities', type=dict, default={True: 0.5, False: 0.0},
                                 help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
             parser.add_argument('--context_size', type=int, default=8, help='dimension of task embedding')
             parser.add_argument('--rank', type=int, default=4)
@@ -125,7 +125,7 @@ class CycleGANModel(BaseModel):
         self.loss_D_B = self.backward_D_basic(self.netD_B, self.real_A, fake_A)
 
     def backward_G(self):
-        lambda_idt = self.opt.lambda_identities[self.task]
+        lambda_idt = self.opt.lambda_identities[self.opt.serial_batches[self.task]]
         lambda_A = self.opt.lambda_A
         lambda_B = self.opt.lambda_B
         # Identity loss
