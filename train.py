8934b2568b79cd702430f2cb1f8d59f53447b7a4
Tingfung Lau <ldf921@126.com>
2018-11-22 01:37:17 +0000

    Fix model saving

--- train.py
+++ train.py
@@ -33,8 +33,8 @@ if __name__ == '__main__':
     visualizer = Visualizer(opt)
     total_steps = 0
 
-    print('Save initialzed models')
-    model.module.save_networks('latest')
+    # print('Save initialzed models')
+    # model.module.save_networks('latest')
 
     for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
         epoch_start_time = time.time()
