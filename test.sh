c885a1964300bf9439ba3bcc8bf00933967724a9
Mengxiong <mliu60@illinois.edu>
2018-11-25 18:04:59 -0500

    test all baselines

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 suffix=_baseline
 for dataset in apple2orange cezanne2photo cityscapes facades horse2zebra maps monet2photo summer2winter_yosemite ukiyoe2photo vangogh2photo; do
-    command="python test.py --dataroot ./datasets/$dataset --name $dataset$suffix --batch_size 1"
+    command="python test.py --model cycle_gan --dataroot ./datasets/$dataset --name $dataset$suffix --batch_size 1"
     echo $command
     $command
 done
--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 
 suffix=_baseline
 for dataset in apple2orange cezanne2photo cityscapes facades horse2zebra maps monet2photo summer2winter_yosemite ukiyoe2photo vangogh2photo; do
-    command="python train.py --dataroot ./datasets/$dataset --name $dataset$suffix --batch_size 5 --lambda_identity 0.1"
+    command="python train.py --model cycle_gan --dataroot ./datasets/$dataset --name $dataset$suffix --batch_size 5 --lambda_identity 0.1"
     echo $command
     $command
 done
